ff7dd232b4960b9d7a217c87d46e7d88b8196d0d
juyelshop <mxnjuyelshop@gmail.com>
2026-04-06 22:24:43 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const app = express();
 
+const VERIFY_TOKEN = "juyel123"; // তুমি যেকোনো নাম দিতে পারো
+
 app.get("/", (req, res) => {
   res.send("WhatsApp Bot Running ✅");
 });
 
+// Webhook verification
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token === VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
 app.listen(3000, () => {
-  console.log("Server running on port 3000");
+  console.log("Server running...");
 });
